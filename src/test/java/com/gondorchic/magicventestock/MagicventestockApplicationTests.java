d58b46792fa3d61fb01b92d61d9e615ae9d2c25c
b.3legolas <b.3legolas@gmail.com>
2024-06-06 07:00:30 -0700

    initialize spring

---
+++ src/test/java/com/gondorchic/magicventestock/MagicventestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gondorchic.magicventestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicventestockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
