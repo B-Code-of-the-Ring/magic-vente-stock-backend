ea51be1755c6c0d9d6cc40a2c2d0266bf03cfb16
Legolas-B <b.3legolas@gmail.com>
2024-06-10 05:22:04 +0300

    delete files

--- src/main/webapp/app/shared/model/produit.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduit {
-  id?: number;
-  libelleProduit?: string;
-  prixProduit?: number | null;
-  quantiteProduit?: number | null;
-  imageProduit?: string | null;
-}
-
-export const defaultValue: Readonly<IProduit> = {};
